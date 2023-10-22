e9e88d04b7bde433f82dc2c3063d52a605abefab
DommSite <johan.johansson06@live.se>
2023-10-22 15:43:37 +0200

    Add project files.

---
+++ Prorammeringsuppgift4.9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace programmeringsinlämning4_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange triangelns sidlängd");
+            try
+            {
+                int sidlängd = int.Parse(Console.ReadLine());
+
+                for (int a = 1; a <= sidlängd; a++)
+                {
+                    for (int b = 1; b <= a; b++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine("");
+                }
+            } 
+            catch{
+                Console.WriteLine("Ingen giltlig siffra");
+            }
+        }
+    }
+}
